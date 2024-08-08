47351887ffce62be299c298250bb396ba6c94cff
wwontk <wwontk@gmail.com>
2024-08-08 22:58:09 +0900

    Build: Tailwind CSS 커스터마이징

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "symbol-pattern": "url('/src/assets/symbol.jpg')",
+      },
+      colors: {
+        "theme-main-color": "#8ec3b1",
+        "theme-color-001": "#def5e5",
+        "theme-color-002": "#bcead5",
+        "theme-color-003": "#9ed5c5",
+      },
+      boxShadow: {
+        "five-percent": "0 4px 4px -1px rgba(12, 12, 13, 0.05)",
+        "ten-percent": "0 4px 4px -1px rgba(12, 12, 13, 0.1)",
+      },
+    },
   },
   plugins: [],
 };
