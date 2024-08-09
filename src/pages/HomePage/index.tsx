d809a3ffe2c9ffdf0d3ef7e55e3d9b627d200bbd
wwontk <wwontk@gmail.com>
2024-08-10 00:04:03 +0900

    Feat: 홈페이지 UI 제작

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,31 @@
+import CategoryList from "../../components/common/CategoryList";
+import PopularCard from "../../components/ListItem/PopularCard";
+
 const HomePage = () => {
   return (
     <>
       <div>
         <section className="w-[1000px] m-auto">
-          <p>홈페이지 입니다.</p>
+          <div className="flex flex-col py-14 gap-8">
+            <p className="font-bold text-2xl text-custom-gray-001">
+              오늘의 인기모임이 모임?
+            </p>
+            <div className="flex justify-between">
+              <PopularCard />
+              <PopularCard />
+              <PopularCard />
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="bg-white">
+        <section className="w-[1000px] m-auto">
+          <div className="flex flex-col py-14 gap-14">
+            <p className="font-bold text-2xl text-custom-gray-001">
+              원하는 모임을 선택하세요
+            </p>
+            <CategoryList />
+          </div>
         </section>
       </div>
     </>
