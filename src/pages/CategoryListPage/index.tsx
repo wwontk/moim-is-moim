15f208522e4eb7e44a1dce7664f4ced8d2ef74d1
wwontk <wwontk@gmail.com>
2024-08-20 17:25:39 +0900

    Feat: 카테고리 Select 옵션별 정렬

--- src/pages/CategoryListPage/index.tsx
+++ src/pages/CategoryListPage/index.tsx
@@ -1,4 +1,4 @@
-import { child, onChildAdded, ref } from "firebase/database";
+import { child, onValue, orderByChild, query, ref } from "firebase/database";
 import MoimCard from "../../components/ListItem/MoimCard";
 import { database } from "../../firebase";
 import { MoimObjectType } from "../../types/Moim";
@@ -8,26 +8,43 @@ const CategoryListPage = () => {
   const dbRef = ref(database);
 
   const [moims, setMoims] = useState<MoimObjectType[]>([]);
+  const [sortedOption, setSortedOption] = useState<string>("all");
 
   const addMoimsListener = useCallback(() => {
-    const moimsArray: Array<MoimObjectType> = [];
+    let moimQuery;
 
-    onChildAdded(child(dbRef, `moims`), (DataSnapshot) => {
-      const moimData = {
-        moimId: DataSnapshot.key,
-        ...DataSnapshot.val(),
-      };
+    if (sortedOption === "popular") {
+      moimQuery = query(child(dbRef, "moims"), orderByChild("views"));
+    } else if (sortedOption === "new") {
+      moimQuery = query(child(dbRef, "moims"), orderByChild("createdAt"));
+    } else {
+      moimQuery = child(dbRef, "moims");
+    }
 
-      moimsArray.push(moimData);
-      const newMoimsArray = [...moimsArray];
-
-      setMoims(newMoimsArray);
+    onValue(moimQuery, (snapshot) => {
+      const moimsArray: Array<MoimObjectType> = [];
+      snapshot.forEach((childSnapshot) => {
+        const moimData = {
+          moimId: childSnapshot.key,
+          ...childSnapshot.val(),
+        };
+        moimsArray.push(moimData);
+      });
+      if (sortedOption === "popular" || sortedOption === "new") {
+        setMoims(moimsArray.reverse());
+      } else {
+        setMoims(moimsArray);
+      }
     });
-  }, [dbRef]);
+  }, [dbRef, sortedOption]);
 
   useEffect(() => {
     addMoimsListener();
   }, [addMoimsListener]);
+
+  const handleSortedChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortedOption(e.target.value);
+  };
 
   const renderMoims = (moims: MoimObjectType[]) => {
     return (
@@ -40,15 +57,20 @@ const CategoryListPage = () => {
     <>
       <div className="flex flex-col gap-4">
         <div className="flex justify-between items-center">
-          <p>체크박스</p>
+          <div className="flex gap-2">
+            <input type="checkbox" name="myregion" id="myregion" />
+            <p>우리 지역 모임</p>
+          </div>
           <select
             name="sort"
             id="sort"
             className="w-28 py-[10px] px-4 border-2 rounded-xl"
+            value={sortedOption}
+            onChange={handleSortedChange}
           >
+            <option value="all">전체</option>
             <option value="popular">인기순</option>
             <option value="new">최신순</option>
-            <option value="deadline">마감순</option>
           </select>
         </div>
         <div className={`flex flex-wrap gap-8`}>{renderMoims(moims)}</div>
