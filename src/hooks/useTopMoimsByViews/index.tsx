c96b47c50c13871e5f7f9f1eb27edfb291b41223
wwontk <wwontk@gmail.com>
2024-08-23 21:09:14 +0900

    Refactor: onValue 사용하여 실시간으로 데이터를 불러오도록 수정

--- src/hooks/useTopMoimsByViews/index.tsx
+++ src/hooks/useTopMoimsByViews/index.tsx
@@ -1,21 +1,28 @@
 import { useEffect, useState } from "react";
 import { MoimObjectType } from "../../types/Moim";
-import { get, limitToLast, orderByChild, query, ref } from "firebase/database";
+import {
+  limitToLast,
+  onValue,
+  orderByChild,
+  query,
+  ref,
+} from "firebase/database";
 import { database } from "../../firebase";
 
 const useTopMoimsByViews = () => {
   const [topMoims, setTopMoims] = useState<MoimObjectType[]>([]);
 
   useEffect(() => {
-    const fetchTopMoims = async () => {
-      try {
-        const moimsRef = ref(database, "moims");
-        const topMoimsQuery = query(
-          moimsRef,
-          orderByChild("views"),
-          limitToLast(3)
-        );
-        const snapshot = await get(topMoimsQuery);
+    const moimsRef = ref(database, "moims");
+    const topMoimsQuery = query(
+      moimsRef,
+      orderByChild("views"),
+      limitToLast(3)
+    );
+
+    const unsubscribe = onValue(
+      topMoimsQuery,
+      (snapshot) => {
         if (snapshot.exists()) {
           const moimList: MoimObjectType[] = Object.keys(snapshot.val()).map(
             (key) => ({
@@ -26,14 +33,15 @@ const useTopMoimsByViews = () => {
           const sortedMoims = moimList.sort((a, b) => b.views - a.views);
           setTopMoims(sortedMoims);
         } else {
-          setTopMoims([]); // 데이터가 없을 때 빈 배열 설정
+          setTopMoims([]);
         }
-      } catch (error) {
-        console.error("Error fetching latest moims:", error);
+      },
+      (error) => {
+        console.error("Error fetching top moims by views:", error);
       }
-    };
+    );
 
-    fetchTopMoims();
+    return () => unsubscribe();
   }, []);
 
   return topMoims;
